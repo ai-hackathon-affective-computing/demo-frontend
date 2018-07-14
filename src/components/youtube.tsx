a20bd4a6dfb9c4b0b977b333f9e26e5719b909c7
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-14 15:07:05 +0200

    :rocket: add yt uris to yt component

--- src/components/youtube.tsx
+++ src/components/youtube.tsx
@@ -4,12 +4,24 @@ import YouTube from "react-youtube"
 import "./youtube.scss"
 
 interface IYoutubeProps {
-  uri: string
+  index?: number
 }
 
 export default class Youtube extends Component<IYoutubeProps> {
+  private uris: string[] = [
+    "", // 0 nothing
+    "apBWI6xrbLY", // 1 beach boys
+    "U5bUmx-hk-c", // 2 summer in the city
+    "haECT-SerHk", // 3 helene fischer
+    "KvRVky0r7YM", // 4 house
+    "nGSOsEfJPO8", // 5 black metal
+    "", // 6
+    "", // 7
+    "", // 8
+    "" // 9
+  ]
 
-  public render({ uri }: IYoutubeProps) {
+  public render({ index }: IYoutubeProps) {
     // tslint:disable-next-line:no-any
     const opts: any = {
       height: "390",
@@ -22,11 +34,11 @@ export default class Youtube extends Component<IYoutubeProps> {
 
     return (
       <div class="youtube">
-      { uri &&
+      { index &&
       <YouTube
-          videoId="nGSOsEfJPO8"
+          videoId={this.uris[index]}
           opts={opts}
-          onReady={() => console.log("YT Ready")}
+          onReady={() => console.log(`YT Ready ${index}`)}
         />
       }
     </div>
