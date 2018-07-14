897884f976829f257fea29c3b1f37e18bbc5f5fb
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-14 14:53:15 +0200

    :fire: hack away spotify

--- src/components/spotify.tsx
+++ src/components/spotify.tsx
@@ -1,21 +1,28 @@
 import { h, Component } from "preact"
+import "./spotify.scss"
 
 const uri = "spotify:album:2GV8iJIzB2Eu8m1FKxYh8u"
 
-export default class Spotify extends Component<{}, {}> {
+interface ISpotifyProps {
+  uri?: string
+}
+
+export default class Spotify extends Component<ISpotifyProps, {}> {
   public render() {
     return <div />
-    // return <div>
-    //    /*<iframe
-    //     title="Spotify"
-    //     className="SpotifyPlayer"
-    //     src={`https://embed.spotify.com/?uri=${uri}`}
-    //     width={400}
-    //     height={400}
-    //     frameBorder="0"
-    //     allow="encrypted-media"
-    //     allowtransparency="true"
-    //   />
-    // </div>
+    /*return <div class="spotify">
+       <iframe
+        id= {"spotify-widget"}
+        title="Spotify"
+        className="SpotifyPlayer"
+        src={`https://embed.spotify.com/?uri=${uri}`}
+        width={400}
+        height={400}
+        frameBorder="0"
+        // @ts-ignore
+        allow="encrypted-media"
+        allowtransparency="true"
+      />
+    </div>*/
   }
 }
