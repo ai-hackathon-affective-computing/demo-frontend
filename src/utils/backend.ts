e9a7ffb7b8077ca821a012f9030c0fd06207d4fc
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-14 14:20:55 +0200

    :hammer: add lastPersonId to backend calls

--- src/utils/backend.ts
+++ src/utils/backend.ts
@@ -6,8 +6,15 @@ const lambdaBaseURL =
   "https://l1zp6t6k1j.execute-api.eu-west-1.amazonaws.com/dev"
 
 export default class Backend {
+  public static lastPersonId: number
+
   public static async getNextAction(state: IActionState) {
-    const qs = stringify(state)
+    const entireState = {
+      ...state,
+      personId: Backend.lastPersonId
+    }
+
+    const qs = stringify(entireState)
     const url = `${lambdaBaseURL}/next_action?${qs}`
     return get(url)
   }
@@ -18,13 +25,18 @@ export default class Backend {
   ): Promise<boolean> {
     // console.log(`Setting in S3: ${key}:${value ? value.toString() : "###"}`)
     try {
-      const bucketURL = await get(`${lambdaBaseURL}/camera/`)
-      // console.log(`bucketURL: ${JSON.stringify(bucketURL)}`)
-      const result = await put(bucketURL.uploadUrl, value ? value : {}, false, {
+      const { uploadUrl, personId } = await get(`${lambdaBaseURL}/camera/`)
+
+      // shame
+      Backend.lastPersonId = personId
+
+      //console.log(`bucketURL: ${JSON.stringify(bucketURL)}`)
+      const result = await put(
+        uploadUrl, value ? value : {}, false, {
         "Content-Type": "image/png",
         "x-amz-acl": "public-read"
       })
-      // console.log(result)
+      //console.log(result)
       return Boolean(result)
     } catch (e) {
       return false
