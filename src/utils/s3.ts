a675f9d6c3e250c66f81c4e6c4576499271066ea
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-14 02:41:06 +0200

    :hammer: change s3 calls to use presigned urls

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,20 +1,22 @@
-import { post, get } from "improved/dist/ajax"
-const bucketURL = ""
+import { get, put } from "improved/dist/ajax"
+
+const lambdaURL = "https://ngstvf71w5.execute-api.eu-west-1.amazonaws.com/dev/camera/"
 
 export default class {
   // tslint:disable-next-line:no-any
   public static async write(key: string, value: Blob | undefined): Promise<boolean> {
     console.log(`Setting in S3: ${key}:${value ? value.toString() : ""}`)
     try {
-      const result = await post(`${bucketURL}${key}`, value ? value : {}, false)
+      const bucketURL = await get(lambdaURL)
+      console.log(`bucketURL: ${JSON.stringify(bucketURL)}`)
+      const result = await put(bucketURL.uploadUrl, value ? value : {}, false, {
+        "Content-Type": "image/png",
+        "x-amz-acl": "public-read"
+      })
       console.log(result)
       return Boolean(result)
     } catch (e) {
       return false
     }
-  }
-
-  public static async read(key: string): Promise<Response> {
-    return get(`${bucketURL}${key}`, {}, true)
   }
 }
