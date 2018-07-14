aba0fddcf0b006e7ef5305b71e1f6333264169f8
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-07-14 14:53:01 +0200

    :wrench: add preact-compat

--- webpack.config.js
+++ webpack.config.js
@@ -50,10 +50,14 @@ const config = (entry, outfile, node = false) => {
     resolve: {
       extensions: [".tsx", ".ts", ".js", ".css", ".scss", ".sass"],
       // Comment this in if preact-compat is necessary
-      /*"alias": {
-        "react": "preact-compat",
-        "react-dom": "preact-compat"
-      }*/
+      "alias": {
+        'react': 'preact-compat',
+        'react-dom': 'preact-compat',
+        // Not necessary unless you consume a module using `createClass`
+        'create-react-class': 'preact-compat/lib/create-react-class',
+        // Not necessary unless you consume a module requiring `react-dom-factories`
+        'react-dom-factories': 'preact-compat/lib/react-dom-factories'
+      }
     },
     plugins: isProduction ? prodPlugins : []
   }
